1bcf63535e201827cceba2b405aaac7b2a75c538
ritaurbani <ritaurbani@gmail.com>
2024-12-03 16:12:00 +0100

    feat: start

---
+++ data/posts.js
@@ -0,0 +1,34 @@
+const posts = [
+    {
+        titolo: "Viaggio in Italia",
+        contenuto: "Esploriamo le bellezze naturali e storiche dell'Italia.",
+        immagine: "img/ciambellone.jpeg",
+        tags: ["Italia", "viaggio"]
+    },
+    {
+        titolo: "Nuove Tecnologie",
+        contenuto: "Scopriamo le tecnologie che definiranno il futuro.",
+        immagine: "img/ciambellone.jpeg",
+        tags: ["tecnologia", "futuro"]
+    },
+    {
+        titolo: "Ricette Semplici",
+        contenuto: "Un piatto veloce e gustoso per ogni occasione.",
+        immagine: "img/ciambellone.jpeg",
+        tags: ["cucina", "ricetta"]
+    },
+    {
+        titolo: "Mindfulness",
+        contenuto: "Tecniche per vivere il presente con maggiore consapevolezza.",
+        immagine: "img/ciambellone.jpeg",
+        tags: ["benessere", "consapevolezza"]
+    },
+    {
+        titolo: "Moda 2024",
+        contenuto: "Le tendenze più in voga per la stagione autunnale.",
+        immagine: "img/ciambellone.jpeg",
+        tags: ["moda", "tendenze"]
+    }
+];
+
+module.exports = posts;
---
+++ routers/posts.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const router = express.Router();
+
+//Index-get - Leggiamo tutti i dati - API per prelevare tutti i dati
+router.get("/", (rq, res)=>{
+    
+})
+
+//Show - get
+
+//Create - post
+
+//Update - put
+
+//Modify - patch
+
+//Destroy - delete
