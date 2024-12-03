afe890a64c89e21b48d6508acce78972a3d65d09
ritaurbani <ritaurbani@gmail.com>
2024-12-03 16:48:08 +0100

    feat: wip

--- app.js
+++ app.js
@@ -1,6 +1,13 @@
+//Inizializzazione express
 const express = require("express");
 const app = express(); 
 const port = 3000;
+
+//Import rotte
+const postsRouter = require("./routers/posts")
+
+//Includo tutte le rotte dei posts con prefisso ppost nelle url di ogni rotta
+app.use("/posts", postsRouter)
 
 app.get("/", (req, res) =>{
     res.send("Hello")
--- routers/posts.js
+++ routers/posts.js
@@ -1,17 +1,37 @@
+//Inizializzazione
 const express = require("express");
 const router = express.Router();
 
+//Export router
+module.exports = router;
+
 //Index-get - Leggiamo tutti i dati - API per prelevare tutti i dati
-router.get("/", (rq, res)=>{
-    
+router.get("/", (req, res)=>{
+    res.send("Server del mio blog")
 })
 
 //Show - get
+router.get("/:id", (req, res)=>{
+    const postId = req.params.id;
+    res.json("Prelevo dettagli di un singolo post" + postId);
+})
 
 //Create - post
-
+router.post("/", (req, res)=>{
+    res.json("qui aggiungo un nuovo post")
+})
 //Update - put
-
+router.put("/:id", (req, res)=>{
+    const postId = req.params.id;
+    res.json("qui aggiorno tutti i dati del post id" + postId)
+})
 //Modify - patch
-
+router.patch("/:id", (req, res)=>{
+    const postId = req.params.id;
+    res.json("qui aggiorno solo alcuni dati del post id" + postId)
+})
 //Destroy - delete
+router.delete("/:id", (req, res)=>{
+    const postId = req.params.id;
+    res.json("qui elimino il post id" + postId) 
+})
