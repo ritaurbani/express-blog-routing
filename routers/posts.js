dca580444391c5474342232ce1af0a2360b427d4
ritaurbani <ritaurbani@gmail.com>
2024-12-03 17:56:08 +0100

    feat: bonus

--- routers/posts.js
+++ routers/posts.js
@@ -11,7 +11,7 @@ const postsList = require("../data/posts")
 //////ROTTE////////
 
 //Index-get - Leggiamo tutti i dati - API per prelevare tutti i dati - Restituisci la lista di dati
-router.get("/", (req, res)=>{
+router.get("/", (req, res) => {
     const result = {
         data: postsList,
         count: postsList.length
@@ -20,27 +20,28 @@ router.get("/", (req, res)=>{
 })
 
 //Show - get
-router.get("/:id", (req, res)=>{
-    const postId = req.params.id;
-    res.json("Prelevo dettagli di un singolo post" + postId);
+router.get("/:id", (req, res) => {
+    const postId = parseInt(req.params.id);
+    const singleElem = postsList.find((curpost) => curpost.id === postId);
+    res.json(singleElem);
 })
 
 //Create - post
-router.post("/", (req, res)=>{
+router.post("/", (req, res) => {
     res.json("qui aggiungo un nuovo post")
 })
 //Update - put
-router.put("/:id", (req, res)=>{
+router.put("/:id", (req, res) => {
     const postId = req.params.id;
     res.json("qui aggiorno tutti i dati del post id" + postId)
 })
 //Modify - patch
-router.patch("/:id", (req, res)=>{
+router.patch("/:id", (req, res) => {
     const postId = req.params.id;
     res.json("qui aggiorno solo alcuni dati del post id" + postId)
 })
 //Destroy - delete
-router.delete("/:id", (req, res)=>{
+router.delete("/:id", (req, res) => {
     const postId = req.params.id;
-    res.json("qui elimino il post id" + postId) 
+    res.json("qui elimino il post id" + postId)
 })
